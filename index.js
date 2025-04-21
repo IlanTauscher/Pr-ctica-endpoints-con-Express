571be285ca415e87e1a2c8e9dbd3ff3236e11a57
IlanTauscher <48240500@est.ort.edu.ar>
2025-04-21 10:26:43 -0300

    Add files via upload
    termine los extras

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Bienvenido a mi servidor');
 });
@@ -14,17 +17,41 @@ app.get('/numero', (req, res) => {
 });
 
 app.get('/usuario', (req, res) => {
-    res.json    ({ "nombre": "Ana", "edad": 25 });
+    res.json({ "nombre": "Ana", "edad": 25 });
 });
 
 app.get('/productos', (req, res) => {
-    res.json    ([ "Mouse", "Teclado", "Monitor"
-]);
+    res.json([ "Mouse", "Teclado", "Monitor"]);
 });
 
 app.get('/materias', (req, res) => {
-    res.json    ([ { "nombre": "Matemática" }, {
-    "nombre": "Lengua" } ]);
+    res.json([ { "nombre": "Matemática" }, { "nombre": "Lengua" } ]);
+});
+
+let personas = [];
+
+app.post('/personas', (req, res) => {
+    const persona = req.body;
+    console.log(persona);
+
+    if(!persona) return res.status(400).json("error catastrofico");
+    personas.push(persona);
+    res.status(201).json({mensaje: "Persona agregada", personas});
+});
+
+app.get('/personas', (req, res) => {
+    res.json(personas);
+});
+
+app.delete('/personas/:indice', (req, res) => {
+    const indice = parseInt(req.params.indice);
+
+    if(isNaN(indice) || !personas[indice]){
+        return res.status(404).json({error: "Persona no encontrada."});
+    }
+
+    const eliminado = personas.splice(indice, 1);
+    res.json({mensaje:`persona elimiada: ${eliminado[0].nombre}, personas`})
 });
 
 app.listen(port, () => {
